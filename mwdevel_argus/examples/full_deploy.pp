f2414080efedea0c06cabdac490678ed8098d046
Marco Caberletti <marco.caberletti@gmail.com>
2017-04-05 16:44:59 +0200

    Add test.vo setup

--- mwdevel_argus/examples/full_deploy.pp
+++ mwdevel_argus/examples/full_deploy.pp
@@ -1,6 +1,8 @@
 include mwdevel_infn_ca
 include mwdevel_test_ca
 include mwdevel_egi_trust_anchors
+include mwdevel_igtf_distribution
+include mwdevel_test_ca_policies
 
 include haveged
 include mwdevel_argus
